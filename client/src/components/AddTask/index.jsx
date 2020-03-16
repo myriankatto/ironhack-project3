3fecf2190c2ddfb24d1abdeb6eddcfcb751bf61e
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-03-16 21:59:40 +0000

    add task form ok - apenas não checked false depois de send

--- client/src/components/AddTask/index.jsx
+++ client/src/components/AddTask/index.jsx
@@ -3,19 +3,30 @@ import { Form, Button } from 'react-bootstrap';
 
 import { create }  from './../../services/task';
 
+
+
+
+
+
 class AddTask extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
       level: '', 
-      urgency:'', 
+      urgency: '',
       category:'',
-      personal:''
+      personal:'',
+      frequency: '',
+      description: '',
+      checked: false
+      
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleFormSubmission = this.handleFormSubmission.bind(this);
+    this.changePersonal = this.changePersonal.bind(this);
+    this.changeUrgency = this.changeUrgency.bind(this);
   }
 
   handleInputChange(event) {
@@ -24,7 +35,26 @@ class AddTask extends Component {
       [name]: value
     });
 
+    
   }
+
+  changePersonal(){
+    this.setState({
+      personal: true
+    });
+  }
+
+  changeUrgency(){
+    this.setState({
+      urgency: true
+    });
+  }
+
+  // changeCheckPersonal(){
+  //   this.setState( previousState => 
+  //      {checkedPersonal: !previousState.checkedPersonal}
+  //     );
+  // }
 
   handleFormSubmission(event) {
     event.preventDefault();
@@ -34,14 +64,17 @@ class AddTask extends Component {
     const urgency = this.state.urgency;
     const personal = this.state.personal;
     const category = this.state.category;
+    const frequency = Number(this.state.frequency);
+    const description = this.state.description;
     
-    //const { name, level, urgency, category, personal  } = this.state;
-    const id = this.props.idWorkspace;
-    console.log(id, name);
+    console.log('STATE', this.state);
+    //console.log('typeof frequency', typeof frequency);
 
+    const id = this.props.idWorkspace;
+   
     //if (!name || !level || !urgency || !category || !personal) return;
     
-    create({id,name})
+    create({id,name, level, urgency, personal, category, frequency, description })
       .then()
       .catch(error => {
         console.log(error);
@@ -50,17 +83,26 @@ class AddTask extends Component {
       this.setState({
         name: '',
         level: '', 
-        urgency:'', 
+        urgency: '',
         category:'',
-        personal:''
+        personal:'',
+        frequency: '',
+        description: '',
+        checked: false
       });
   };
+
+ 
+    
+  
+
 
   render() {
     return (
       <Form onSubmit={this.handleFormSubmission}>
         <Form.Group controlId="formBasicName">
 
+          {/*Task's name */}
           <Form.Control
             type="text"
             name="name"
@@ -70,34 +112,68 @@ class AddTask extends Component {
             autoComplete="off"
           />
 
-          {/* <Form.Control
-            type="text"
+          {/* Frequency */}
+         <Form.Control
+            type="number"
+            name="frequency"
+            value={this.state.frequency}
+            onChange={this.handleInputChange}
+            placeholder="Set frequency"
+            autoComplete="off"
+          /> 
+
+          {/* Level */}
+          <Form.Label>Example select</Form.Label>
+          <Form.Control 
+            as="select"
             name="level"
             value={this.state.level}
-            onChange={this.handleInputChange}
-            placeholder="Your level"
-            autoComplete="off"
-          /> */}
+            onChange={this.handleInputChange}>
+            <option>easy</option>
+            <option>medium</option>
+            <option>hard</option>
+          </Form.Control>
 
-          {/* <Form.Control
-            type="text"
-            name="personal"
-            value={this.state.personal}
-            onChange={this.handleInputChange}
-            placeholder="Personal"
-            autoComplete="off"
-          /> */}
+          {/* Pessoal ou Geral*/}
+          <Form.Check 
+            type="checkbox"
+            label="Personal"
+            //checked={this.state.checked}
+            isSelected={this.changePersonal}
+            onChange={this.changePersonal}
+          />
 
-          {/* <Form.Label>Description</Form.Label>
+          {/* Checkbox para Urgência*/}
+          <Form.Check 
+            type="checkbox"
+            label="Urgency"
+            //checked={this.state.checked}
+            isSelected={this.changeUrgency}
+            onChange={this.changeUrgency}
+          />
+
+           {/*Category*/}
+          <Form.Label>Select the Category</Form.Label>
           <Form.Control 
-          as="textarea" 
-          rows="3"
-          type="text"
-          name="description"
-          value={this.state.description}
-          onChange={this.handleInputChange}
-          placeholder="Your description"
-          autoComplete="off" /> */}
+            as="select"
+            name="category"
+            value={this.state.category}
+            onChange={this.handleInputChange}>
+              <option>Kitchen</option>
+              <option>Laundry</option>
+              <option>Clean up</option>
+          </Form.Control>
+
+          <Form.Label>Description</Form.Label>
+          <Form.Control 
+            as="textarea" 
+            rows="3"
+            type="text"
+            name="description"
+            value={this.state.description}
+            onChange={this.handleInputChange}
+            placeholder="Your description"
+            autoComplete="off" />
           
 
           
