53ee2db5cf57b35fc8e32fc956d85cd2f617aa4a
Myrian Katto <myriankatto@gmail.com>
2020-03-16 11:13:18 +0000

    commit before merge

---
+++ client/src/components/CreateWorkspace/index.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+class CreateWorkspace extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      workspaceName: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormSubmission = this.handleFormSubmission.bind(this);
+  }
+
+  handleInputChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleFormSubmission(event) {
+    event.preventDefault();
+    const name = this.state.workspaceName;
+    if (!name) return;
+    const workspace = {
+      name
+    };
+    this.props.addTask(workspace);
+    this.setState({
+      workspaceName: ''
+    });
+  }
+  render() {
+    return (
+      <Form onSubmit={this.handleFormSubmission}>
+        <Form.Group controlId="formBasicName">
+          <Form.Control
+            type="text"
+            name="workspaceName"
+            value={this.state.workspaceName}
+            onChange={this.handleInputChange}
+            placeholder="Create a new Workspace..."
+            autoComplete="off"
+          />
+          <Button variant="primary" type="submit">
+            +
+          </Button>
+        </Form.Group>
+      </Form>
+    );
+  }
+}
+
+export default CreateWorkspace;
--- client/src/views/workspaceDashboard/index.jsx
+++ client/src/views/workspaceDashboard/index.jsx
@@ -1,8 +1,73 @@
 import React, { Component } from 'react';
 
+import * as workspaceService from './services/workspace';
+
+import CreateWorkspace from '../../components/CreateWorkspace';
+// import NavBar from '../../components/NavBar';
+// import SearchWorkspace from '../../components/serchWorkspace';
+
 class WorkspaceDashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      workspaces: [],
+      query: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleWorkspaceAddition = this.handleWorkspaceAddition.bind(this);
+  }
+
+  handleInputChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  // componentDidMount() {
+  //   this.listWorkspaces();
+  // }
+
+  // listWorkspaces() {
+  //   workspaceService
+  //     .list()
+  //     .then(workspaces => {
+  //       this.setState({
+  //         workspaces
+  //       });
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     });
+  // }
+
+  handleWorkspaceAddition(workspace) {
+    workspaceService
+      .create(workspace)
+      .then(data => {
+        this.setState(previousState => ({
+          workspaces: [data, ...previousState.workspace]
+        }));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  // get filteredWorkspaces() {
+  //   const filteredWorkspaces = this.state.workspace.filter(workspace => {
+  //     return workspace.name.toLowerCase().includes(this.state.query.toLowerCase());
+  //   });
+  //   return filteredWorkspaces;
+  // }
+
   render() {
-    return <div>DASHBOARD</div>;
+    return (
+      <div>
+        <CreateWorkspace addWorkspace={this.handleWorkspaceAddition} />
+        {/* <WorkspaceList workspaces={this.filteredWorkspaces} removeTask={this.handleTaskRemoval} /> */}
+      </div>
+    );
   }
 }
 
