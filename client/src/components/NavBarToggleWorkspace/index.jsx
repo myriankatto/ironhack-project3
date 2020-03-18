0094caf5d9e99668807802ce7d9f1e90f9883078
TemidoRocha <manueltemidorocha@gmail.com>
2020-03-18 13:17:08 +0000

    style workshop toggle menu

--- client/src/components/NavBarToggleWorkspace/index.jsx
+++ client/src/components/NavBarToggleWorkspace/index.jsx
@@ -24,21 +24,45 @@ class NavBarToggleWorkspace extends Component {
     return (
       <Swipeable onSwipedLeft={this.props.handleMouseDown}>
         <div id="flyoutMenu" className={visibility}>
-          <h2>
-            <a onMouseDown={this.props.handleMouseDown}>Go Back by click or swipe left</a>
-          </h2>
-          <h2>
-            <Link to={'/editWorkspace'}>Edit Workspace</Link>
-          </h2>
-          <h2>
-            <Link to={'/listWorkspaceUsers'}>List of Users</Link>
-          </h2>
-          <h2>
-            <a href="#">Share Workspace</a>
-          </h2>
-          <h2>
-            <a href="#">Upgrade</a>
-          </h2>
+          <nav className="buttonPlusMenu">
+            <a href="#" onMouseDown={this.props.handleMouseDown}>
+              <img
+                style={{
+                  width: '2em',
+                  float: 'left',
+                  position: 'relative',
+                  margin: '2em 1em'
+                }}
+                src="./../images/left-white.svg"
+                alt="go back icon"
+              />
+            </a>
+
+            <h2>
+              <img src="./../images/workspaceConfig.svg" alt="go back icon" />
+              <Link style={{ color: 'white' }} to={'/editWorkspace'}>
+                Edit Workspace
+              </Link>
+            </h2>
+            <h2>
+              <img src="./../images/list.svg" alt="go back icon" />
+              <Link style={{ color: 'white' }} to={'/listWorkspaceUsers'}>
+                List of Users
+              </Link>
+            </h2>
+            <h2>
+              <img src="./../images/share-svgrepo-com.svg" alt="go back icon" />
+              <Link style={{ color: 'white' }} to={'/'}>
+                Share Workspace
+              </Link>
+            </h2>
+            <h2>
+              <img src="./../images/premium-svgrepo-com.svg" alt="go back icon" />
+              <Link style={{ color: 'white' }} to={'/'}>
+                Upgrade
+              </Link>
+            </h2>
+          </nav>
         </div>
       </Swipeable>
     );
