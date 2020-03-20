1083256f3ce3c5edb1a6431abacfd0831569da3c
TemidoRocha <manueltemidorocha@gmail.com>
2020-03-20 12:38:10 +0000

    qrcode assigned workspace for approval route done

--- client/src/components/RouteShareWorkspace/index.jsx
+++ client/src/components/RouteShareWorkspace/index.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import { askPermissionWorkspace } from './../../services/workspaceUser';
+import { Link } from 'react-router-dom';
+import { single } from '../../services/workspace';
 
 class RouteShareWorkpace extends Component {
-  // componentDidMount() {
-  //   console.log(this.props.user._Id, this.props.workspaceId);
-  //   askPermissionWorkspace(this.props.user._Id, this.props.workspaceId)
-  //     .then(x => console.log(x))
-  //     .catch(error => console.log(error));
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      workspaceName: ''
+    };
+  }
+  componentDidMount() {
+    const workspaceId = this.props.history.location.pathname.split('/')[2];
+    askPermissionWorkspace(this.props.user._id, workspaceId).catch(error => console.log(error));
+    single(workspaceId).then(workspace =>
+      this.setState({ workspaceName: workspace.workspace.name })
+    );
+  }
 
   render() {
-    console.log('share: ' + this.props.user);
     return (
       <div>
-        {/* <h3>{this.props.user.name} been sucessufly added to the {this.props.user._Id}, {this.props.workspaceId}</h3> */}
+        <h3>
+          {this.props.user.name} been sucessufly added to the {this.state.workspaceName}
+        </h3>
         <p>Please wait for administarator approval.</p>
+        <Link to={'/'}>Return</Link>
       </div>
     );
   }
