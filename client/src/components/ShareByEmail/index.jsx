937f9c79fe1658ae4c6a14a094660255f2008d9f
TemidoRocha <manueltemidorocha@gmail.com>
2020-04-02 17:25:08 +0100

    email is being sent and it disapears the message success or fail after 3s

--- client/src/components/ShareByEmail/index.jsx
+++ client/src/components/ShareByEmail/index.jsx
@@ -13,8 +13,11 @@ class ShareByEmail extends Component {
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleEmail = this.handleEmail.bind(this);
+    this.resetEmailMessage = this.resetEmailMessage.bind(this);
   }
-
+  resetEmailMessage() {
+    this.setState({ emailMessage: '' });
+  }
   handleInputChange(event) {
     const { name, value } = event.target;
     this.setState({
@@ -26,10 +29,22 @@ class ShareByEmail extends Component {
       this.props.workspace._id,
       this.props.workspace.name,
       this.state.query
-    ).then(message => this.setState({ emailMessage: message.msg }));
+    ).then(message => {
+      const resetEmailMessage = this.resetEmailMessage;
+      this.setState({ emailMessage: message.msg.toString() });
+      setTimeout(function() {
+        resetEmailMessage();
+      }, 3000);
+    });
   }
 
   render() {
+    //not working, doesn't recognize the message
+    let messageColor = 'emailMessageRed';
+    this.emailMessage === 'success'
+      ? (messageColor = 'emailMessageGreen')
+      : (messageColor = 'emailMessageRed');
+    console.log(messageColor);
     return (
       <React.Fragment>
         <div className="workspace__search__form">
@@ -45,10 +60,10 @@ class ShareByEmail extends Component {
             />
           </form>
         </div>
-        <article>{this.state.emailMessage}</article>
         <button className="workspace__btn" onClick={this.handleEmail}>
           Share Workspace
         </button>
+        <article id={messageColor}>{this.state.emailMessage}</article>
       </React.Fragment>
     );
   }
